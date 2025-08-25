c89d5e66dee9cb8bd238e0871e6e0735e1436df9
Olfa Karoui <olfaakaroui@gmail.com>
2025-08-25 17:30:23 +0200

    Enable pre and Post execution hooks on the step executor level

--- gateway.go
+++ gateway.go
@@ -27,6 +27,8 @@ type Gateway struct {
 	queryerFactory     *QueryerFactory
 	queryPlanCache     QueryPlanCache
 	locationPriorities []string
+	preExecutionHook   StepHook
+	postExecutionHook  StepHook
 
 	// group up the list of middlewares at startup to avoid it during execution
 	requestMiddlewares  []graphql.NetworkMiddleware
@@ -86,6 +88,8 @@ func (g *Gateway) Execute(ctx *RequestContext, plans QueryPlanList) (map[string]
 		RequestMiddlewares: g.requestMiddlewares,
 		Plan:               plan,
 		Variables:          ctx.Variables,
+		PreExecutionHook:   g.preExecutionHook,
+		PostExecutionHook:  g.postExecutionHook,
 	}
 
 	// TODO: handle plans of more than one query
@@ -291,6 +295,21 @@ func WithLocationPriorities(priorities []string) Option {
 		g.locationPriorities = priorities
 	}
 }
+
+// WithPreExecutionHook returns an Option that sets a pre-execution hook for all steps
+func WithPreExecutionHook(hook StepHook) Option {
+	return func(g *Gateway) {
+		g.preExecutionHook = hook
+	}
+}
+
+// WithPostExecutionHook returns an Option that sets a post-execution hook for all steps
+func WithPostExecutionHook(hook StepHook) Option {
+	return func(g *Gateway) {
+		g.postExecutionHook = hook
+	}
+}
+
 
 // WithLogger returns an Option that sets the logger of the gateway
 func WithLogger(l Logger) Option {
